c58ddb95bc72bab6265ba69977e601420db2f1b5
weitian93 <63578580+weitian93@users.noreply.github.com>
2020-04-17 09:25:50 +0800

    fail

--- AspNetCoreWebServiceTest/Controllers/HelloControllerTest.cs
+++ AspNetCoreWebServiceTest/Controllers/HelloControllerTest.cs
@@ -11,7 +11,7 @@ namespace AspNetCoreWebServiceTest.Controllers
             HelloController controller = new HelloController();
             var response = controller.Get().Value as Response;
             Assert.Equal("Hello World!", response.Output);
-            //assertFalse("fail",false);
+            assertFalse("fail",false);
         }
 
         [Theory]
